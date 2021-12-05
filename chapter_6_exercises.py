c4164c6c38e888de6a4fb01d66a4b9c9ca8d97e8
Matthew R <matthew@mjrouser.com>
2021-12-05 18:22:06 -0500

    first 3 exercises

--- chapter_6_exercises.py
+++ chapter_6_exercises.py
@@ -2,10 +2,24 @@
 
 #Exercise 1
 
-index = 5
-fruit = 'banana'
+#fruit = 'banana'
+#index = len(fruit) - 1
 
-while index < len(fruit) and index > -1:
-    letter = fruit[index]
-    print(letter)
-    index = index - 1 
+#while index < len(fruit) and index > -1:
+#    letter = fruit[index]
+#    print(letter)
+#    index = index - 1 
+
+#Exercise 2
+#print(fruit[:])
+
+#Exercise 3
+
+word = 'banana'
+def count ():
+    increment = 0
+    for letter in word:
+        if letter == 'a':
+            increment = increment +1
+    print(increment)
+count()
