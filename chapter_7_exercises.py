a32cf58b7595b1911dd6080d20ebc7fa7a75f00c
Matthew R <matthew@mjrouser.com>
2021-12-11 16:08:17 -0500

    ch 7 exercises and files

--- chapter_7_exercises.py
+++ chapter_7_exercises.py
@@ -2,3 +2,30 @@
 
 #Exercise 1
 
+#fhand = open('mbox-short.txt')
+#for line in fhand:
+#    line = line.upper()
+#    print(line)
+
+fname = input('Enter a file to open:')
+try:
+    fhand = open(fname)
+except:
+    if fname == 'na na na boo boo':
+        print('Yeah, well na na na boo boo to you too!')
+    elif fname == 'meh':
+        print("Yeah, that's how I feel about it too")
+    else:
+        print('Could not locate file: ' + fname)
+    exit()
+
+count = 0
+total = 0
+for line in fhand:
+    if line.startswith('X-DSPAM-Confidence:'):
+        spampos = line.find(':')
+        spam = line[spampos +1:]
+        count = count + 1
+        total = total + float(spam)
+spamavg = total / count
+print(spamavg)
