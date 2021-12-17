3d2146bd032bd66a4a49052a537d633eaaa86d7f
Matthew R <matthew@mjrouser.com>
2021-12-16 22:57:38 -0500

    exercise 2

--- chapter_9_exercises.py
+++ chapter_9_exercises.py
@@ -11,11 +11,34 @@ def exercise1 ():
         splitLine = line.split()
         for word in splitLine:
             count += 1
-            myDict.update({count: word})
+            myDict.update({word: count})
             #print('Debug for myDict instantiation:',myDict)
-    vals = list(myDict.values())
+    keyList = list(myDict.keys())
     #print('Debug for count', count)
-    #print(vals)
-    print('Writing' in vals)
+    #print(keyList)
+    print('Writing' in keyList)
 
-exercise1()
+#exercise1()
+
+#Exercise 2
+
+def dictFunc():
+    import string
+    try:
+        fileInput = input('Enter the name of the file you want to open:')
+        fhandle = open(fileInput)
+    except:
+        print('Cannot open file as input')
+    
+    daysCount = dict()
+
+    for line in fhandle:
+        words = line.split()
+        #print(words)
+        if words == [] or words[0] != 'From' : continue
+        #print(words) #mbox-short.txt
+        day = words[2]
+        daysCount[day] = daysCount.get(day,0) + 1
+    print(daysCount)
+
+dictFunc()
